0e278f96bb2bd314d48c04da5422da91bf14406e
Hamzah Fayad <hamzahfayad99@gmail.com>
2021-10-30 09:56:08 +0200

    bg

---
+++ main.js
@@ -0,0 +1,16 @@
+/*shared scripts*/
+
+/*let el = document.querySelector(".navi a.link--active");
+console.log(el.innerHTML);
+
+if (el.innerHTML === "Destination") {
+  document.body.style.backgroundImage =
+    "url('../assets/destination/background-destination-desktop.jpg')";
+} else if (el.innerHTML === "Crew") {
+  document.body.style.backgroundImage =
+    "url('../assets/crew/background-crew-desktop.jpg')";
+} else if (el.innerHTML === "Technology") {
+  document.body.style.backgroundImage =
+    "url('../assets/technology/background-technology-desktop.jpg')";
+}
+*/
